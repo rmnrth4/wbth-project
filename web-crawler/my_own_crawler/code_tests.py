1d74a64e43b90b4ceb9d7ca779cd7fa0073cee54
rmnrth4 <rothr8@bfh.ch>
2024-03-04 21:45:58 +0100

    first try mongo

--- web-crawler/my_own_crawler/code_tests.py
+++ web-crawler/my_own_crawler/code_tests.py
@@ -1,3 +1,17 @@
-url = "https://www.mobiliar.ch/versicherungen-und-vorsorge/fahrzeuge-und-reisen/reiseversicherung/"
+import re
 
-print(url.split("/")[-1])
+pattern = re.compile(r"^\s*$")
+
+# Beispiel Verwendung:
+strings = ["", "    ", "\t", "  abc ", "123", "\n", "\n\t  ", "\n\n\n\n\n"]
+
+chained_text = ""
+for s in strings:
+    if not pattern.match(s):
+        chained_text += s
+print(chained_text)
+
+# for item in list_of_text_elements.extract():
+#     if not pattern.match(s):
+
+# return chained_text
